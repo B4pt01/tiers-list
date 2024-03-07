e96e023d333360f6db6d1dfdc58797045133be31
baptiste vauthier <ba.vauthier@gmail.com>
2024-03-07 16:45:06 +0100

    css

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
-let mealCounter = 0;
+// let mealCounter = 0;
 
 const searchMeal = async recherche => {
 	const api_res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${recherche}`);
 	const api_ret = await api_res.json();
 	api_ret.meals.forEach(async (meal, index) => {
-		mealCounter++;
-		const uniqueId = `meal_${mealCounter}`;
-		var img = `<div draggable="true" ondragstart="drag(event)" id="${uniqueId}">
+		// mealCounter++;
+		// const uniqueId = `meal_${mealCounter}`;
+		var img = `<div draggable="true" ondragstart="drag(event)" id="${meal.idMeal}">
         <img src="${meal.strMealThumb}" alt="repas" />
         <p>${meal.strMeal}</p>
     </div>`;
@@ -15,6 +15,12 @@ const searchMeal = async recherche => {
 		console.log(api_ret);
 	});
 };
+
+// const select = ev => {
+// 	var selection = document.getElementById('selection');
+// 	selection.appendChild(ev.target);
+// };
+// (a ajouter dans la div pour l'img) onclick="select(event)"
 
 const validerForm = () => {
 	document.getElementById('img').innerHTML = '';
