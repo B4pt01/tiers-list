9df0369af11b90a57163b92399d6940b70a5af3b
baptiste vauthier <ba.vauthier@gmail.com>
2024-03-07 12:03:19 +0100

    ajout des noms

--- script/script.js
+++ script/script.js
@@ -2,7 +2,10 @@ const searchMeal = async recherche => {
 	const api_res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${recherche}`);
 	const api_ret = await api_res.json();
 	api_ret.meals.forEach((meal, index) => {
-		var img = `<img src="${meal.strMealThumb}" alt="repas" draggable="true" ondragstart="drag(event)" id="${index}">`;
+		var img = `<div draggable="true" ondragstart="drag(event)" id="${index}">
+        <img src="${meal.strMealThumb}" alt="repas" />
+        <p>${meal.strMeal}</p>
+    </div>`;
 		document.getElementById('img').innerHTML += img;
 		console.log(meal.strMealThumb);
 	});
@@ -13,15 +16,3 @@ const validerForm = () => {
 	var nom = document.getElementById('recherches').value;
 	searchMeal(nom);
 };
-
-function dragstartHandler(ev) {
-	// Add the target element's id to the data transfer object
-	ev.dataTransfer.setData('text/plain', ev.target.id);
-}
-
-window.addEventListener('DOMContentLoaded', () => {
-	// Get the element by id
-	const element = document.getElementById('p1');
-	// Add the ondragstart event listener
-	element.addEventListener('dragstart', dragstartHandler);
-});
