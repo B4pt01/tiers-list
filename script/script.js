d0e436a765a98fd60085ba463598b414f227927b
baptiste vauthier <ba.vauthier@gmail.com>
2024-03-07 10:19:12 +0100

    recuperation des img

--- script/script.js
+++ script/script.js
@@ -4,6 +4,8 @@ const searchMeal = async (recherche) => {
   );
   const api_ret = await api_res.json();
   api_ret.meals.forEach((meal) => {
+    var img = `<img src="${meal.strMealThumb}" alt="repas"></img>`;
+    document.getElementById("img").innerHTML += img;
     console.log(meal.strMealThumb);
   });
 };
